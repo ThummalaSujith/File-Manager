c703e12bbe79bed4aa4950504d6e240221862fba
Thummala Sujith <srt7@njit.edu>
2024-03-12 19:52:05 -0400

    created Header component and styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Header.css";
+import { CiSearch } from "react-icons/ci";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="combine">
+
+          <div className="header-logo">
+            <img src="./Fm.png" alt="file-manager"/>
+            <p>Internet Download Manager 10.36</p>
+            </div>
+
+        <div className="header-menu">
+          <span>Tasks</span>
+          <span>File</span>
+          <span>Downloads</span>
+          <span>View</span>
+          <span>Help</span>
+        </div>
+
+        </div>
+
+        
+
+      
+      <div className="search">
+        <div className="search-container">
+          <CiSearch className="search-icon" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search in the List"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
